ac725347666fb8a8dde43a4e1f9f321a70051af6
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 10:58:18 +0300

    Commits

---
+++ social_justice_app/lib/frontend/widgets/main_build_button_widget.dart
@@ -0,0 +1,22 @@
+ import 'package:flutter/material.dart';
+
+Widget buildOptionButtonMain(BuildContext context,
+        {required IconData icon,
+        required String label,
+        required String route,
+        required Color color}) {
+      return ElavatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white, backgroundColor: color,
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20));
+          elevation: 8.0,
+        ),
+        icon: Icon(icon, size: 28),
+        label: Text(
+          label,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        onPressed: ()
+      );
+        }
