6df7cf31eda95359da8b1868592b06f6f97d57a7
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 09:15:43 +0300

    Commits

--- social_justice_app/lib/frontend/classes/report_class.dart
+++ social_justice_app/lib/frontend/classes/report_class.dart
@@ -1,2 +1,26 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+
+class ReportDetailsScreen extends StatelessWidget {
+  final Map<String, dynamic> report;
+
+  const ReportDetailsScreen({super.key, required this.report});
+
+  bool _isValidUrl(String url) {
+    final uri = Uri.tryParse(url);
+    return uri != null && (uri.isScheme('http') || uri.isScheme('https'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(report['title'] ?? 'Report Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+      )
+    )
+  }
+}
