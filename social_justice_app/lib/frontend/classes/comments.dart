56b853dacb1ccd589904834bce6cee295d22d13b
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 07:45:22 +0300

    Commits

---
+++ social_justice_app/lib/frontend/classes/comments.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommentsScreen extends StatefulWidget {
+  final int reportId;
+
+  const CommentsScreen({super.key required thos.reportId});
+
+  @override
+  _CommentScreenState createState() => _CommentScreenState();
+}
