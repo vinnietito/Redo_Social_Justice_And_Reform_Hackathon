7230d27ca813fc408f4eac7517641cf343c35598
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 11:46:14 +0300

    Commits

--- social_justice_app/lib/frontend/widgets/main_drawer_widget.dart
+++ social_justice_app/lib/frontend/widgets/main_drawer_widget.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:social_justice_app/frontend/widgets/main_drawer_item.dart';
 import 'package:solop/frontend/widgets/main_drawer_Item.dart';
+Widget mainDrawer(BuildContext context) {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsects.zero,
+      children: [
+        const UserAccountsDrawerHeader(
+          accountName: Text(''),
+          accountEmail: Text(''),
+          currentAccountPicture: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/logo.png'),
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+          decoration: BoxDecoration(color: Colors.deepPurpleAccent),
+           ),
+           buildDrawerItem(
+              context,
+              icon: Icons.search,
+              label: 'Fact Check',
+              route: '/facts',
+             ),
+
+      ],
+    ),
+  )
+}
