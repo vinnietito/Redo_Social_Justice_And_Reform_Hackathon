52f479fa4e0913a03d665bbae6435013e7bdce4b
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 08:32:05 +0300

    Commits

---
+++ social_justice_app/lib/frontend/classes/article_class.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+//Article Detail Screen
+class ArticleDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> article;
+
+  const ArticleDetailScreen({super.key, required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Article Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:  SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                article['tittle'] ?? 'No Title',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    )
+  }
+}
