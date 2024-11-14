41aa7a034b23263ffdbdb71172d9a4c011cff195
vinnietito <vincentnjuguna69@gmail.com>
2024-11-14 08:02:20 +0300

    Commits

--- social_justice_app/lib/frontend/classes/comments.dart
+++ social_justice_app/lib/frontend/classes/comments.dart
@@ -8,3 +8,44 @@ class CommentsScreen extends StatefulWidget {
   @override
   _CommentScreenState createState() => _CommentScreenState();
 }
+
+class _CommentScreenState extends State<CommentsScreen> {
+  final list<String> comments = [];
+  final textEditingController commentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(child: Text('Comments')),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: comments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(comments[index]),
+                );
+              }, 
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: commentController,
+                    ),
+                  )
+                ],
+              )
+            )
+        ],
+      ),
+    )
+  }
+}
