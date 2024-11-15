8d6f0857f97caee8520b3c8108d08bf965311eb1
vinnietito <vincentnjuguna69@gmail.com>
2024-11-15 10:06:17 +0300

    Commits

--- social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
+++ social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:solop/frontend/screens/main_options_screen.dart';
 import 'package:solop/frontend/screens/splash_screen.dart';
+
+class SplashScreenWrapper extends StatefulWidget {
+  const SplashScreenWrapper({super.key});
+
+  @override
+  _SplashScreenWrapperState extends 
+}
