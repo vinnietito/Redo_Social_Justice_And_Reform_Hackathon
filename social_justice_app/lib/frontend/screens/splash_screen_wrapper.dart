c6cf9934cc32cf6304b7a96b9e1d1d63d770fb15
vinnietito <vincentnjuguna69@gmail.com>
2024-11-15 10:35:09 +0300

    Commits

--- social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
+++ social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:solop/frontend/screens/main_options_screen.dart';
-import 'package:solop/frontend/screens/splash_screen.dart';
+import 'package:social_justice_app/frontend/screens/main_options_screen.dart';
+import 'package:social_justice_app/frontend/screens/splash_screen.dart';
 
 class SplashScreenWrapper extends StatefulWidget {
   const SplashScreenWrapper({super.key});
 
   @override
-  _SplashScreenWrapperState createState() => _SplashScreenWrapperState();
-}
-
-class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
-  bool _isLoading = true;
- 
-  @override
-  void initState() {
-    super.initState();
-    _loadData(); // Simulating data loading
-  }
-
-  Future<void> _loadData() async {
-    // Simulate some loading time
-    await Future.delayed(const Duration(seconds: 5));
-
-    // After loading data, update the state to switch to the HomeScreen
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _isLoading ? const SplashScreen() : const MainOptionScreen();
-  }
+  _SplashScreenWrapperState extends 
 }
