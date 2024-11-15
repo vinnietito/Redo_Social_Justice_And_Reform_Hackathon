fbcd4f85ad46fa4fa15b6dc063dd11dac44305c4
vinnietito <vincentnjuguna69@gmail.com>
2024-11-15 10:33:37 +0300

    Commits

--- social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
+++ social_justice_app/lib/frontend/screens/splash_screen_wrapper.dart
@@ -6,5 +6,30 @@ class SplashScreenWrapper extends StatefulWidget {
   const SplashScreenWrapper({super.key});
 
   @override
-  _SplashScreenWrapperState extends 
+  _SplashScreenWrapperState createState() => _SplashScreenWrapperState();
+}
+
+class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
+  bool _isLoading = true;
+ 
+  @override
+  void initState() {
+    super.initState();
+    _loadData(); // Simulating data loading
+  }
+
+  Future<void> _loadData() async {
+    // Simulate some loading time
+    await Future.delayed(const Duration(seconds: 5));
+
+    // After loading data, update the state to switch to the HomeScreen
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading ? const SplashScreen() : const MainOptionScreen();
+  }
 }
