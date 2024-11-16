141c47bc6f723efcd6fe7faf73c0882a6f0e872f
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 09:00:46 +0300

    Commits

---
+++ social_justice_app/lib/frontend/screens/fact_check/fact_check.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:social_justice_app/frontend/classes/article_class.dart';
+import 'dart:convert';
+
+import 'package:social_justice_app/frontend/widgets/fact_build_button_widget.dart';
