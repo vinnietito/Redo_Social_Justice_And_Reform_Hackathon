56cb424f730da4d29694b51abd97b572b5c01281
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 08:22:28 +0300

    Commits

--- social_justice_app/lib/frontend/screens/awareness_solutions/awareness_and_solutions.dart
+++ social_justice_app/lib/frontend/screens/awareness_solutions/awareness_and_solutions.dart
@@ -16,4 +16,38 @@ class _AwarenessAndSolutionsScreenState
     _solutionController.dispose();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colorrs.deepPurpleAccent,
+        title: const Center(child: Text('Awareness & Solutions')),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.blueAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              //Awareness Section
+              buildAwarenessSection(),
+              const SizedBox(height: 20),
+
+              //Solution submission Section
+              buildAwarenessSection(),
+              const SizedBox(height: 20),
+
+              //View Suggested Solutions Sections
+            ],
+          ),),
+      ),
+    )
+  }
     }
