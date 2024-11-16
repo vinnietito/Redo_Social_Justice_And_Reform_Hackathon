9dece59f6c2363bcdfc5b17d5b1280841d212fc8
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 09:38:36 +0300

    Commits

---
+++ social_justice_app/lib/frontend/screens/policies/policy_tracking_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PolicyTrackingScreen extends StatelessWidget {
+  //Sample data for promises, legislation and reforms
+  final List<Map<String, String>> promises = [
+    {
+      'title': 'Youth Employment Program',
+      'status': 'In Progress',
+      'link': 'https://example.com'
+    },
+    {
+      'title': 'Youth Employment Program',
+      'status': 'In Progress',
+      'link': 'https://example.com'
+    }
+  ]
+}
