01031635a985ef759a34d061d35ddc23e0c5dfd2
vinnietito <vincentnjuguna69@gmail.com>
2024-11-16 17:28:49 +0300

    Commits

--- social_justice_app/lib/frontend/screens/report/other_corruption_reports.dart
+++ social_justice_app/lib/frontend/screens/report/other_corruption_reports.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:social_justice_app/frontend/classes/comments_section.dart';
-import 'package:social_justice_app/frontend/classes/detailed_report_class.dart';
+import 'package:social_justice_app/frontend/classes/comments.dart';
+import 'package:social_justice_app/frontend/classes/report_class.dart';
 import 'dart:convert';
 import 'dart:io';
 
